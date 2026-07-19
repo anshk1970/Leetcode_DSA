38cbcbdb725ce7eb2039f788057db77c124fe564
Ansh Kushwaha <anshkushwaha1970@gmail.com>
2026-07-19 09:54:59 +0530

    Time: 13 ms (73.90%) | Memory: 48.8 MB (13.17%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -3,17 +3,16 @@ class Solution {
         int n = arr.length;
         int[] nge = new int[n];
         Stack<Integer> st = new Stack<>();
-        for (int i = 2 * n - 1; i >= 0; i--) {
-            while (st.size() > 0 && st.peek() <= arr[i % n]) {
-                st.pop();
-            }
-            if (i < n) {
-                if (st.size() == 0) nge[i] = -1;
-                else nge[i] = st.peek();
-            }
-            
-            st.push(arr[i % n]);
+        for(int i =n-1;i>=0;i--){
+            st.push(arr[i]);
         }
-        return nge;
+        for(int i=n-1;i>=0;i--){
+            while(st.size()>0 && arr[i]>=st.peek()) st.pop();
+            if(st.size()==0) nge[i] = -1;
+            else nge[i] = st.peek();
+            st.push(arr[i]);
+        }
+        
+     return nge;
     }
 }
