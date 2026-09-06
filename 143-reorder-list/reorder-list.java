f96fdf58c6e8035dd4a28ecfedf962468d783fe2
Ansh Kushwaha <anshkushwaha1970@gmail.com>
2026-09-06 11:33:15 +0530

    Time: 1 ms (99.82%) | Memory: 49.2 MB (71.50%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,32 +1,37 @@
 class Solution {
     public void reorderList(ListNode head) {
         if (head == null || head.next == null) return;
+        //Find The Middle
         ListNode slow = head;
         ListNode fast = head;
-        while (fast != null && fast.next != null) {
+        while(fast!=null && fast.next!=null){
             slow = slow.next;
-            fast = fast.next.next;
+            fast=fast.next.next;
         }
         ListNode head2 = slow.next;
-        slow.next = null; 
-        ListNode prev = null;
+        //Detach The Two LL
+        slow.next = null;
+        //Reverse The Second List
         ListNode curr = head2;
-        while (curr != null) {
-            ListNode nextTemp = curr.next;
+        ListNode prev= null;
+        ListNode fwd = head2;
+        while(curr!=null){
+            fwd = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = nextTemp;
+            curr = fwd;
         }
-        head2 = prev;
-        ListNode first = head;
-        while (head2 != null) {
-            ListNode temp1 = first.next;
-            ListNode temp2 = head2.next;
-            first.next = head2; 
-            head2.next = temp1; 
-            first = temp1;
-            head2 = temp2;
+        //Insert Nodes Of Second List Into the First List
+        ListNode t1 = head;
+        ListNode t2 = prev;
+        while(t2!=null){
+         ListNode m1 = t1.next;
+         ListNode m2 = t2.next;
+         t1.next = t2;
+         t2.next = m1;
+         t1 = m1;
+         t2 = m2; 
         }
-  
+
     }
 }
