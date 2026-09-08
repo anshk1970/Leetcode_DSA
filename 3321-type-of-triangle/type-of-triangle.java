250a22380ddd99246509e39ed9175b3f63f2f6a7
Ansh Kushwaha <anshkushwaha1970@gmail.com>
2026-09-08 20:32:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (93.19%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -3,16 +3,14 @@ class Solution {
         int a = nums[0];
         int b = nums[1];
         int c = nums[2];
-
-        if (a + b <= c || a + c <= b || b + c <= a) {
+        if (a + b <= c || a + c <= b || b + c <= a)
             return "none";
-        }
-        if (a == b && b == c) {
+        if (a == b && b == c)
             return "equilateral";
-        } else if (a == b || b == c || a == c) {
+        else if (a == b || b == c || a == c)
             return "isosceles";
-        } else {
+        else
             return "scalene";
-        }
+
     }
 }
